fc87033d3f19d36874a1d64799e1b2db0513a05d
Fanwise <fanwise.901@gmail.com>
2017-08-01 10:56:28 +0800

    Initial Commit

---
+++ MockTwitterUI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MockTwitterUI
+//
+//  Created by Wei Fan on 01/08/2017.
+//  Copyright © 2017 Wei Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MockTwitterUI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MockTwitterUI
+//
+//  Created by Wei Fan on 01/08/2017.
+//  Copyright © 2017 Wei Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
