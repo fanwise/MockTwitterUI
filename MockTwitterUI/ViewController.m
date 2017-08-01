6c62859b2c50d46ab332e2d58e8c7c9b1e7abb79
Fanwise <fanwise.901@gmail.com>
2017-08-01 16:51:48 +0800

    set up storyboard

--- MockTwitterUI/ViewController.m
+++ MockTwitterUI/ViewController.m
@@ -7,8 +7,21 @@
 //
 
 #import "ViewController.h"
+#import "FXBlurView.h"
 
-@interface ViewController ()
+const CGFloat offset_HeaderStop = 40.0; // At this offset the Header stops its transformations
+const CGFloat offset_B_LabelHeader = 95.0; // At this offset the Black label reaches the Header
+const CGFloat distance_W_LabelHeader = 35.0; // The distance between the bottom of the Header and the top of the White Label
+
+@interface ViewController () <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIView *header;
+@property (weak, nonatomic) IBOutlet UIImageView *headerImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *avatar;
+@property (weak, nonatomic) IBOutlet UIButton *follow;
+@property (weak, nonatomic) IBOutlet UILabel *headerLabel;
+@property (weak, nonatomic) IBOutlet FXBlurView *headerBlurImageView;
 
 @end
 
@@ -16,14 +29,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 
 @end
